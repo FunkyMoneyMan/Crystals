6a83fdd53a73adc07dc7c50da6018b0008fc90a0
FunkyMoneyMan <35664800+FunkyMoneyMan@users.noreply.github.com>
2019-01-20 18:12:27 -0800

    Update CommandCrystal.java
    Made it nicer

--- CommandCrystal.java
+++ CommandCrystal.java
@@ -8,31 +8,26 @@ import org.bukkit.entity.Player;
 
 
 public class CommandCrystal extends CrystalFunction implements CommandExecutor {
-
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("crystal")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                if (sender.hasPermission("crystal.give")) {
-                    if (args.length != 2) {
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[!] &cInvalid arguments. Use /crystal <player> <amount>"));
-                        return true;
-                    }
-
-                    if (Bukkit.getPlayer(args[0]) != null) {
-                        try {
-                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aGiven " + args[1] + " &acrystals to " + args[0]));
-                            giveCrystal(Bukkit.getPlayer(args[0]), Integer.parseInt(args[1]));
-                        } catch (NumberFormatException e) {
-                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[!] &cThe number you inserted was not a valid number"));
-                        }
-                    } else {
-                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[!] &cThat player is not online"));
-
-                    }
-                } else { sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[!] &cYou do not have permission!")); }
+        if (!cmd.getName().equalsIgnoreCase("crystal")){
+            return true;}
+            if (!(sender instanceof Player) && !sender.hasPermission("crystal.give")) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[!] &cYou do not have permission!"));
+            return true;}
+            if (args.length != 2) {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[!] &cInvalid arguments. Use /crystal <player> <amount>"));
+                return true;
             }
-        }return true;
+            if (Bukkit.getPlayer(args[0]) != null) {
+                try {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aGiven " + args[1] + " &acrystals to " + args[0]));
+                    giveCrystal(Bukkit.getPlayer(args[0]), Integer.parseInt(args[1]));
+                } catch (NumberFormatException e) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[!] &cThe number you inserted was not a valid number"));
+                }
+            } else {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&l[!] &cThat player is not online"));
+               }return true;
     }
 }
 
